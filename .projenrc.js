6db0a1d7f0f8de07b424fc179112c75dcf01de53
Ruben Rangel <rubenmrangel@gmail.com>
2023-03-05 10:42:38 -0800

    ci: enable Dependabot

--- .projenrc.js
+++ .projenrc.js
@@ -2,6 +2,7 @@ const { typescript } = require('projen');
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'scryscript',
+  dependabot: true,
   jestOptions: {
     configFilePath: 'jest.config.json',
   },
