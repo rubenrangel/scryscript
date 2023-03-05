96eb8001f309fc3e684a439615f47cb85e6b271a
Ruben Rangel <rubenmrangel@gmail.com>
2023-03-05 08:58:07 -0800

    chore: add .idea files to source control

--- .projenrc.js
+++ .projenrc.js
@@ -10,5 +10,27 @@ const project = new typescript.TypeScriptProject({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+
+  gitignore: [
+    // https://raw.githubusercontent.com/github/gitignore/main/Global/JetBrains.gitignore
+    // Covers JetBrains IDEs: IntelliJ, RubyMine, PhpStorm, AppCode, PyCharm, CLion, Android Studio, WebStorm and Rider
+    // Reference: https://intellij-support.jetbrains.com/hc/en-us/articles/206544839
+    '.idea/**/workspace.xml',
+    '.idea/**/tasks.xml',
+    '.idea/**/usage.statistics.xml',
+    '.idea/**/dictionaries',
+    '.idea/**/shelf',
+    '.idea/**/contentModel.xml',
+    '.idea/**/dataSources/',
+    '.idea/**/dataSources.ids',
+    '.idea/**/dataSources.local.xml',
+    '.idea/**/sqlDataSources.xml',
+    '.idea/**/dynamic.xml',
+    '.idea/**/uiDesigner.xml',
+    '.idea/**/dbnavigator.xml',
+    '*.iws',
+    'out/',
+    '.idea/httpRequests',
+  ],
 });
 project.synth();
