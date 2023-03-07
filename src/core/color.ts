264e317b5fd6e29b0913a958319149f971b92034
Ruben Rangel <rubenmrangel@gmail.com>
2023-03-06 18:19:32 -0800

    feat(color): add Color enum

---
+++ src/core/color.ts
@@ -0,0 +1,7 @@
+export enum Color {
+  WHITE = 'W',
+  BLUE = 'U',
+  BLACK = 'B',
+  RED = 'R',
+  GREEN = 'G',
+}
---
+++ test/core/color.test.ts
@@ -0,0 +1,15 @@
+import { Color } from '../../src/core/color';
+
+describe('Color', () => {
+  test.each(
+    [
+      [Color.WHITE, 'W'],
+      [Color.BLUE, 'U'],
+      [Color.BLACK, 'B'],
+      [Color.RED, 'R'],
+      [Color.GREEN, 'G'],
+    ],
+  )('enum %s equals %s', (enumValue, expectedValue) => {
+    expect(enumValue).toBe(expectedValue);
+  });
+});
