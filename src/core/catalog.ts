ebf394f04f7891ed95a304ed7834ee2555b93506
Ruben Rangel <rubenmrangel@gmail.com>
2023-03-06 18:52:52 -0800

    feat(catalog): add Catalog interface

---
+++ src/core/catalog.ts
@@ -0,0 +1,23 @@
+/**
+ * A Catalog object contains an array of Magic datapoints (words, card values,
+ * etc). Catalog objects are provided by the API as aids for building other
+ * Magic software and understanding possible values for a field on Card objects.
+ *
+ * @see https://scryfall.com/docs/api/catalogs
+ */
+export interface Catalog {
+  /**
+   * A link to the current catalog on Scryfall’s API.
+   */
+  uri: string;
+
+  /**
+   * The number of items in the `data` array.
+   */
+  totalValues: number;
+
+  /**
+   * An array of datapoints.
+   */
+  data: string[];
+}
