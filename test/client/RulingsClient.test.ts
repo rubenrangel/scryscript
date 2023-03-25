6673cca8f0338c6d564216c3b45bcc5a7f9339e2
Ruben Rangel <rubenmrangel@gmail.com>
2023-03-25 09:03:32 -0700

    feat(rulings client)!: move `RulingsClient` request classes and interfaces to `RulingsClient.ts`

--- test/client/RulingsClient.test.ts
+++ test/client/RulingsClient.test.ts
@@ -1,7 +1,53 @@
 import { beforeAll, describe, expect, Mock, test, vi } from "vitest";
-import { RulingsClient } from "../../src/client/RulingsClient";
+import {
+  ArenaIdRulingsRequest,
+  CardIdRulingsProps,
+  MtgoFoilIdRulingsRequest,
+  MtgoIdRulingsRequest,
+  MultiverseIdRulingsRequest,
+  RulingsClient,
+  SetCodeCollectorNumberRulingsProps,
+} from "../../src/client/RulingsClient";
 import { IScryfallList } from "../../src/core/IList";
 import { Ruling } from "../../src/core/Ruling";
+
+describe("CardIdRulingsProps", () => {
+  test("it returns the correct URL", () => {
+    expect(new CardIdRulingsProps({ cardId: "1234" }).url).toEqual("/cards/1234/rulings");
+  });
+});
+
+describe("SetCodeCollectorNumberRulingsProps", () => {
+  test("it returns the correct URL", () => {
+    expect(new SetCodeCollectorNumberRulingsProps({ setCode: "mm1", collectorNumber: "1234" }).url).toEqual(
+      "/cards/mm1/1234/rulings"
+    );
+  });
+});
+
+describe("MtgoIdRulingsRequest", () => {
+  test("it returns the correct URL", () => {
+    expect(new MtgoIdRulingsRequest({ mtgoId: "1234" }).url).toEqual("/cards/mtgo/1234/rulings");
+  });
+});
+
+describe("MtgoFoilIdRulingsRequest", () => {
+  test("it returns the correct URL", () => {
+    expect(new MtgoFoilIdRulingsRequest({ mtgoFoilId: "1234" }).url).toEqual("/cards/mtgo/1234/rulings");
+  });
+});
+
+describe("MultiverseIdRulingsRequest", () => {
+  test("it returns the correct URL", () => {
+    expect(new MultiverseIdRulingsRequest({ multiverseId: 1234 }).url).toEqual("/cards/multiverse/1234/rulings");
+  });
+});
+
+describe("ArenaIdRulingsRequest", () => {
+  test("it returns the correct URL", () => {
+    expect(new ArenaIdRulingsRequest({ arenaId: 1234 }).url).toEqual("/cards/arena/1234/rulings");
+  });
+});
 
 describe("RulingsClient", () => {
   describe("listRulings", () => {
